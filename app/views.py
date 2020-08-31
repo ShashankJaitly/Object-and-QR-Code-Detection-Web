d9e23ba82899a47cd1b0eecb1eb1d98395f234e3
ShashankJaitly <shashank16vasu@gmail.com>
2020-08-31 17:14:15 +0530

    --refactor Videofeed, StreamView

--- app/views.py
+++ app/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import StreamingHttpResponse
-from app.Integrations import get_frame
+from app.Integrations import VideoStream
 
 def index(request):
     return render(request, 'app/index.html')
 
-def StreamView(Integrations):
+def StreamView(object):
     while True:
-        frame = Integrations.get_frame()
+        frame = object.get_frame()
         yield(b'--frame\r\n'
             b'content-type: image\r\n\r\n' + frame + b'\r\n\r\n')
 
 def VideoFeed(request):
-    return StreaminHttpResponse(StreamView(), content_type = 'multipart/x-mixed-replace; boundary=frame')
+    return StreamingHttpResponse(StreamView(VideoStream()), content_type = 'multipart/x-mixed-replace; boundary=frame')
 
